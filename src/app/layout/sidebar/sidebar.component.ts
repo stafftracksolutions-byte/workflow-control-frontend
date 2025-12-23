7635564f8e11e46354367fc03f554def5acdcbfb
Melkin Andres Villamil Melo <mavillamilme56@soy.sena.edu.co>
2025-12-23 11:29:27 -0500

    Frontend: Sidebar automatico segun rol

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,17 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './sidebar.component.html'
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
   auth = inject(AuthService);
-
-  hasRole(role: string) {
-    return this.auth.user?.rol === role;
-  }
 }
